d4cc065dc82ad1c0eb5131f601c037ecc7f4044d
Pan Karapas <23283805+PanKarapas@users.noreply.github.com>
2026-04-17 19:31:02 +0100

    feat: add initial stats panel

--- src/app/pages/participant-results/participant-results.ts
+++ src/app/pages/participant-results/participant-results.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { GlobalParticipantStats, MessageStats } from '../../../model/message-stats';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatDividerModule } from '@angular/material/divider';
 
 import { addIndividualCounts, individualCountToFrequencyRows } from '../../../utils/individual-counts-utils';
 import { FrequencyRow } from '../../../model/shared-ui-types';
@@ -16,7 +17,7 @@ type BubbleDataPointWithCount = { x: string, y: string, r: number; count: number
 Chart.register(BubbleController, LinearScale, PointElement, CategoryScale, Tooltip, Title);
 @Component({
   selector: 'app-participant-results',
-  imports: [MatTabsModule, FrequencyRowTable],
+  imports: [MatTabsModule, FrequencyRowTable, MatDividerModule],
   templateUrl: './participant-results.html',
   styleUrl: './participant-results.scss',
 })
@@ -24,6 +25,7 @@ export class ParticipantResults implements OnInit, OnDestroy {
   wordFrequencyDataSource: MatTableDataSource<FrequencyRow> = new MatTableDataSource([] as FrequencyRow[]);
   reactionFrequencyDataSource: MatTableDataSource<FrequencyRow> = new MatTableDataSource([] as FrequencyRow[]);
   chart!: Chart<"bubble", BubbleDataPointWithCount[], string>;
+  globalParticipantStats?: GlobalParticipantStats;
 
   constructor(private router: Router, private route: ActivatedRoute) { }
 
@@ -52,9 +54,10 @@ export class ParticipantResults implements OnInit, OnDestroy {
       .reduce((acc, curr) => addIndividualCounts(acc, curr), new Map());
 
     this.reactionFrequencyDataSource = new MatTableDataSource(individualCountToFrequencyRows(individualReactionCount));
-    const globalParticipantData = data.global[participantName];
-    if (globalParticipantData) {
-      this.createChartTimeGraph(globalParticipantData);
+    this.globalParticipantStats = data.global[participantName];
+
+    if (this.globalParticipantStats) {
+       this.createChartTimeGraph();
     }
   }
 
@@ -62,7 +65,11 @@ export class ParticipantResults implements OnInit, OnDestroy {
     this.chart?.destroy();
   }
 
-  createChartTimeGraph(data: GlobalParticipantStats) {
+  createChartTimeGraph() {
+    const data = this.globalParticipantStats;
+    if(!data){
+      throw new Error("Called createChartTimeGraph with undefined globalParticipantStats");
+    }
     const dayOfTheWeekLabels = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
     const timeOfDayLabels = [
       "12 am", "1 am", "2 am", "3 am", "4 am", "5 am", "6 am", "7 am", "8 am", "9 am", "10 am", "11 am",
